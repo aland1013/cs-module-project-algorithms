89082c8012e35dc97d261bbea54f0f634d4f41c0
aland1013 <adam.land@gmail.com>
2020-10-08 09:13:37 -0400

    implemented solution to product_of_all_other_numbers.py with O(n) time complexity

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,16 +3,50 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    result = []
+    # first pass solution with O(n^2) time complexity
+    # result = []
     
+    # for i in range(len(arr)):
+    #     product = 1
+    #     for j in range(len(arr)):
+    #         if i != j:
+    #             product *= arr[j]
+    #     result.append(product)
+    
+    # return result
+    
+    # optimized solution with O(n) time complexity
+    # traversing the arr three times is O(n) + O(n) + O(n)
+    # which is O(3n) which is considered O(n)
+    
+    # for each number in arr, find the product of
+    # all the numbers before it and store it in a new array
+    products_before_i = [0 for _ in range(len(arr))]
+    product = 1
     for i in range(len(arr)):
-        product = 1
-        for j in range(len(arr)):
-            if i != j:
-                product *= arr[j]
-        result.append(product)
+        products_before_i[i] = product
+        product *= arr[i]
     
-    return result
+    # for each number in arr, find the product of
+    # all the numbers after it and store it in a new array
+    # easy - just traverse arr backwards
+    products_after_i = [0 for _ in range(len(arr))]
+    product = 1
+    for i in range(len(arr) - 1, -1, -1):
+        products_after_i[i] = product
+        product *= arr[i]
+    
+    # for each number in arr, multiply the product of
+    # all the numbers before it by the product of
+    # all the numbers after it
+    products_except_i = [0 for _ in range(len(arr))]
+    product = 1
+    for i in range(len(arr)):
+        products_except_i[i] = products_before_i[i] * products_after_i[i]
+    
+    return products_except_i
+        
+    
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
